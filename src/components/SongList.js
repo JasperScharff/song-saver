a3195877201c9a0c12ab255f8d51708b60ef609f
Jasper Scharff <jas-scharff@live.nl>
2022-11-23 17:22:54 +0100

    Update

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function SongList(props) {
-    return (
-        <div>
-            {/* <p>{props.}</p> */}
-        </div>
-    );
+  return (
+    <div>
+      <p>{props.songs}</p>
+      <p>{props.artists}</p>
+      <p>{props.genres}</p>
+      <p>{props.ratings}</p>
+    </div>
+  );
 }
 
 export default SongList;
