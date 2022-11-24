ca585f20e7e798cf15b96abbd2ba9c4095e9b144
Jasper Scharff <jas-scharff@live.nl>
2022-11-24 12:22:00 +0100

    only onClick and Inline Styling

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+// Inline h3
+const inline = {
+  display: "inline",
+  margin: 85,
+};
+
 function SongForm(props) {
   return (
     <div>
@@ -7,26 +13,55 @@ function SongForm(props) {
         <thead>
           <tr className="song-header">
             <th className="song-row__item">
+              
               <input
                 type="text"
                 value={props.songs}
                 name="songs"
                 placeholder="Song..."
+                onChange={props.addSong}
               ></input>
             </th>
+
             <th className="song-row__item">
-              <input type="text" placeholder="Artist"></input>
+              <input
+                type="text"
+                value={props.artists}
+                name="artists"
+                placeholder="Artist..."
+                onChange={props.addArtist}
+              ></input>
             </th>
+
             <th className="song-row__item">
-              <input type="text" placeholder="Genre"></input>
+              <input
+                type="text"
+                value={props.genres}
+                name="songs"
+                placeholder="Genres..."
+                onChange={props.addGenre}
+              ></input>
             </th>
+
             <th className="song-row__item">
-              <input type="text" placeholder="Rating"></input>
+              <input
+                type="text"
+                value={props.ratings}
+                name="ratings"
+                placeholder="Rating..."
+                onChange={props.addRating}
+              ></input>
             </th>
           </tr>
         </thead>
       </table>
       <button onClick={props.addSong}>Add Song</button>
+
+      <h3 style={inline}>Song</h3>
+      <h3 style={inline}>Artist</h3>
+      <h3 style={inline}>Genre</h3>
+      <h3 style={inline}>Rating</h3>
+
     </div>
   );
 }
