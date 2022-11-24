102a3b863e49f464b5408161399944f9082f5d13
Jasper Scharff <jas-scharff@live.nl>
2022-11-24 12:22:05 +0100

    only onClick and Inline Styling

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,24 @@
 import React from "react";
 
+const stylingForSongListDiv = {
+  fontSize: 20,
+  //   lineHeight: 2,
+  margin: 5,
+};
+
+const newRow = {
+  color: "green",
+  marginLeft: 150,
+  marginRight: 40,
+};
+
 function SongList(props) {
   return (
-    <div>
-      <p>{props.songs}</p>
-      <p>{props.artists}</p>
-      <p>{props.genres}</p>
-      <p>{props.ratings}</p>
+    <div style={stylingForSongListDiv}>
+      <span style={newRow}>{props.songs}</span>
+      <span style={newRow}>{props.artists}</span>
+      <span style={newRow}>{props.genres}</span>
+      <span style={newRow}>{props.ratings}</span>
     </div>
   );
 }
