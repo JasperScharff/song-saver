ebe3e9231acf60f2db893f9f9cbfa8386558ecc2
Jasper Scharff <jas-scharff@live.nl>
2022-11-24 12:22:10 +0100

    only onClick and Inline Styling

--- src/components/SongOverview.js
+++ src/components/SongOverview.js
@@ -6,26 +6,51 @@ class SongOverview extends Component {
   constructor() {
     super();
     this.state = {
-      songs: ["f "],
-      artists: [" "],
-      genres: [" "],
-      ratings: [" "],
+      songs: [""],
+      artists: [""],
+      genres: [""],
+      ratings: [""],
     };
     this.addSong = this.addSong.bind(this);
   }
 
   addSong = (song) => {
-    // console.log("OK")
     this.setState({
       songs: song.target.value,
     });
   };
+  addArtist = (artist) => {
+    this.setState({
+      artists: artist.target.value,
+    });
+  };
+  addGenre = (genre) => {
+    this.setState({
+      genres: genre.target.value,
+    });
+  };
+  addRating = (rating) => {
+    this.setState({
+      ratings: rating.target.value,
+    });
+  };
+
 
   render() {
     return (
       <div>
-        <SongForm addSong={this.addSong} />
-        <SongList addSong={this.addSong} />
+        <SongForm 
+          addSong={this.addSong} 
+          addArtist={this.addArtist} 
+          addGenre={this.addGenre} 
+          addRating={this.addRating} 
+          />
+        <SongList 
+          songs={this.state.songs}
+          artists={this.state.artists}
+          genres={this.state.genres}
+          ratings={this.state.ratings}
+        />
       </div>
     );
   }
